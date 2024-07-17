7e38febda73717d98d4a65580b14c38ff398c8c7
Paul Bowen-Huggett <paulhuggett@mac.com>
2024-07-17 15:14:20 +0200

    Replace the mcoded7 implementation.

--- include/mcoded7.h
+++ include/mcoded7.h
@@ -1,59 +1,215 @@
-/**********************************************************
- * MIDI 2.0 Library
- * Author: Andrew Mee
- *
- * MIT License
- * Copyright 2021 Andrew Mee
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- *
- * ********************************************************/
-
 #ifndef MC7_H
 #define MC7_H
 
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cstddef>
 #include <cstdint>
+#include <iterator>
+
+// MIDI Mcoded7 Encoding
+// ~~~~~~~~~~~~~~~~~~~~~
+// Each group of seven stored bytes is transmitted as eight bytes. First, the
+// sign bits of the seven bytes are sent, followed by the low-order 7 bits of
+// each byte. (The reasoning is that this would make the auxiliary bytes appear
+// in every 8th byte without exception, which would therefore be slightly easier
+// for the receiver to decode.)
+//
+// The seven bytes:
+//     AAAAaaaa BBBBbbbb CCCCcccc DDDDdddd EEEEeeee FFFFffff GGGGgggg
+// are sent as:
+//     0ABCDEFG
+//     0AAAaaaa 0BBBbbbb 0CCCcccc 0DDDdddd 0EEEeeee 0FFFffff 0GGGgggg
+//
+// From a buffer to be encoded, complete groups of seven bytes are encoded into
+// groups of eight bytes. If the buffer size is not a multiple of seven, there
+// will be some number of bytes leftover after the groups of seven are encoded.
+// This short group is transmitted similarly, with the sign bits occupying the
+// most significant bits of the first transmitted byte. For example:
+//     AAAAaaaa BBBBbbbb CCCCcccc
+// are transmitted as:
+//     0ABC0000 0AAAaaaa 0BBBbbbb 0CCCcccc
+
+namespace mcoded7 {
+
+class encoder {
+public:
+  /// The maximum number of output bytes that can be generated from a single
+  /// call to parse_byte().
+  static constexpr auto max_size = std::size_t{8};
+
+  encoder() { this->reset(); }
+
+  /// \tparam OutputIterator  An output iterator type to which bytes can be written.
+  /// \param value  The value to be encoded.
+  /// \param out  An output iterator to which the output sequence is written.
+  /// \returns  Iterator one past the last element assigned.
+  template <typename OutputIterator>
+  OutputIterator parse_byte(std::uint8_t const value, OutputIterator out);
+
+  /// Call once the entire input sequence has been fed to encoder::parse_byte().
+  /// This function flushes any remaining buffered output.
+  ///
+  /// \tparam OutputIterator  An output iterator type to which bytes can be written.
+  /// \param out  An output iterator to which the output sequence is written.
+  /// \returns  Iterator one past the last element assigned.
+  template <typename OutputIterator> OutputIterator flush(OutputIterator out);
+
+  /// All input is good for encoding, so this function always returns true.
+  constexpr bool good() const { return true; }
+
+  /// Resets the internal state.
+  ///
+  /// This function places the class into a clean state to enable encoding of a
+  /// fresh data stream without having to rescan the input.
+  void reset() {
+    pos_ = 0U;
+    buffer_[0] = 0U;  // reset the MSB for the next block of 7.
+  }
+
+private:
+  std::uint8_t pos_ = 0;
+  std::array<std::uint8_t, max_size> buffer_;
+};
+
+class decoder {
+public:
+  /// The maximum number of output bytes that can be generated from a single
+  /// call to parse_byte().
+  static constexpr auto max_size = std::size_t{1};
+
+  decoder() noexcept { this->reset(); }
+
+  /// \tparam OutputIterator  An output iterator type to which bytes can be written.
+  /// \param value  The value to be decoded.
+  /// \param out  An output iterator to which the output sequence is written.
+  /// \returns  Iterator one past the last element assigned.
+  template <typename OutputIterator>
+  OutputIterator parse_byte(std::uint8_t const value, OutputIterator out);
+
+  /// Call once the entire input sequence has been fed to decoder::parse_byte().
+  /// This function flushes any remaining buffered output.
+  ///
+  /// \tparam OutputIterator  An output iterator type to which bytes can be written.
+  /// \param out  An output iterator to which the output sequence is written.
+  /// \returns  Iterator one past the last element assigned.
+  template <typename OutputIterator> OutputIterator flush(OutputIterator out) {
+    return out;
+  }
+
+  /// Returns true if the input was valid Mcoded7, false otherwise.
+  constexpr bool good() const noexcept { return !static_cast<bool>(bad_); }
+
+  void reset() noexcept {
+    pos_ = msbs_byte_pos_;
+    bad_ = 0U;
+  }
+
+private:
+  /// The value of 'pos_' when the MSB byte is next.
+  static constexpr auto msbs_byte_pos_ = 7U;
+
+  std::uint8_t msbs_ =
+      0U;  ///< The most significant bigs of the current group of bytes.
+  ///< Position within the current group of bytes (starting at 7 and counting
+  ///< down).
+  std::uint8_t pos_ : 3;
+  std::uint8_t bad_ : 1;  ///< 1 if bad input was detected, 0 otherwise.
+};
+
+//*  _            _                   _        _   _           *
+//* (_)_ __  _ __| |___ _ __  ___ _ _| |_ __ _| |_(_)___ _ _   *
+//* | | '  \| '_ \ / -_) '  \/ -_) ' \  _/ _` |  _| / _ \ ' \  *
+//* |_|_|_|_| .__/_\___|_|_|_\___|_||_\__\__,_|\__|_\___/_||_| *
+//*         |_|                                                *
+template <typename OutputIterator>
+OutputIterator encoder::parse_byte(std::uint8_t const value,
+                                   OutputIterator out) {
+  assert(pos_ < 7U && "on entry, pos_ must be in the range [0,7)");
+  static constexpr auto msb = 0x80U;
+  ++pos_;
+  // Remember the most significant bit in buffer[0].
+  buffer_[0] |= (value & msb) >> pos_;
+  // Record the remaining 7 bits in the current buffer position.
+  buffer_[pos_] = value & ~msb;
+  if (pos_ >= max_size - 1U) {
+    out = this->flush(out);
+  }
+  return out;
+}
+
+template <typename OutputIterator>
+OutputIterator encoder::flush(OutputIterator out) {
+  if (pos_ > 0U) {
+    auto const first = std::begin(buffer_);
+    out = std::copy(first, first + pos_ + 1, out);
+    this->reset();
+  }
+  return out;
+}
+
+template <typename OutputIterator>
+OutputIterator decoder::parse_byte(std::uint8_t const value,
+                                   OutputIterator out) {
+  if (pos_ == msbs_byte_pos_) {
+    // This the the byte that encodes the sign bits of the seven following
+    // bytes.
+    msbs_ = value;
+  } else {
+    // The MSB should not be set in mcoded7 data.
+    bad_ |= static_cast<std::uint8_t>(value >> 7U);
+
+    // Assemble the output byte from ths input value and its most-significant
+    // sign bit stored in msbs_.
+    *(out++) =
+        value | ((static_cast<std::uint8_t>(msbs_ >> pos_) & 0x01U) << 7U);
+  }
+  // Decrement pos. If pos is 0 on entry, it will wrap back to msbs_byte_pos_.
+  --pos_;
+  return out;
+}
+
+}  // end namespace mcoded7
 
 class mcoded7Decode {
-private:
-  uint8_t dumpPos = 255;
-  uint8_t fBit = 0;
-  uint8_t cnt = 0;
-  uint8_t bits = 0;
-
 public:
-  uint8_t dump[7];
-  uint16_t currentPos();
-  void reset();
-  void parseS7Byte(uint8_t s7Byte);
+  std::array<std::uint8_t, 7> dump;
+
+  constexpr uint16_t currentPos() const { return pos_; }
+  void reset() {
+    std::fill(std::begin(dump), std::end(dump), std::uint8_t{0});
+    decoder_.reset();
+    pos_ = 0;
+  }
+  void parseS7Byte(uint8_t s7Byte) {
+    pos_ = decoder_.parse_byte(s7Byte, &dump[pos_]) - dump.data();
+  }
+
+private:
+  mcoded7::decoder decoder_;
+  std::uint8_t pos_ = 0;
 };
 
 class mcoded7Encode {
-private:
-  uint16_t dumpPos = 1;
-  uint8_t cnt = 6;
-
 public:
-  uint8_t dump[8];
-  uint16_t currentPos();
-  void reset();
-  void parseByte(uint8_t s8Byte);
+  std::array<std::uint8_t, 8> dump;
+
+  constexpr std::uint16_t currentPos() const { return pos_; }
+
+  void reset() {
+    std::fill(std::begin(dump), std::end(dump), std::uint8_t{0});
+    pos_ = 1;
+    encoder_.reset();
+  }
+
+  void parseByte(std::uint8_t const s8Byte) {
+    pos_ = encoder_.parse_byte(s8Byte, &dump[pos_]) - dump.data();
+  }
+
+private:
+  mcoded7::encoder encoder_;
+  std::uint16_t pos_ = 0;
 };
 
-#endif
+#endif  // MC7_H
---
+++ unittests/test_mcoded7.cpp
@@ -0,0 +1,141 @@
+#include "mcoded7.h"
+
+// standard library
+#include <algorithm>
+#include <cstddef>
+#include <iterator>
+#include <vector>
+
+// google mock/test/fuzz
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#if defined(MCODED7_FUZZTEST) && MCODED7_FUZZTEST
+#include <fuzztest/fuzztest.h>
+#endif
+
+namespace {
+
+struct raw_and_encoded {
+  std::vector<std::uint8_t> raw;
+  std::vector<std::uint8_t> encoded;
+};
+
+class Mcoded7 : public testing::TestWithParam<raw_and_encoded> {
+public:
+  /// Takes a vector of bytes and returns the mcoded7 encoded equivalent vector.
+  static std::vector<std::uint8_t> encode(
+      std::vector<std::uint8_t> const& input) {
+    mcoded7::encoder encoder;
+    std::vector<std::uint8_t> output;
+    auto out = std::back_inserter(output);
+    std::for_each(std::begin(input), std::end(input),
+                  [&encoder, &out](std::uint8_t const b) {
+                    out = encoder.parse_byte(b, out);
+                  });
+    encoder.flush(out);
+    return output;
+  }
+
+  /// Decodes a vector of mcoded7 bytes.
+  static std::vector<std::uint8_t> decode(
+      std::vector<std::uint8_t> const& input) {
+    mcoded7::decoder decoder;
+    std::vector<std::uint8_t> output;
+    auto out = std::back_inserter(output);
+    std::for_each(std::begin(input), std::end(input),
+                  [&decoder, &out](std::uint8_t const b) {
+                    out = decoder.parse_byte(b, out);
+                  });
+    decoder.flush(out);
+    return output;
+  }
+};
+
+}  // end anonymous namespace
+
+// NOLINTNEXTLINE
+TEST_P(Mcoded7, Encode) {
+  auto const& param = GetParam();
+  EXPECT_THAT(encode(param.raw), testing::ContainerEq(param.encoded));
+}
+// NOLINTNEXTLINE
+TEST_P(Mcoded7, Decode) {
+  auto const& param = GetParam();
+  EXPECT_THAT(decode(param.encoded), testing::ContainerEq(param.raw));
+}
+
+namespace {
+
+// A small collection of test vectors.
+raw_and_encoded const empty;
+raw_and_encoded const four{
+    std::vector<std::uint8_t>{0b00010010, 0b00110100, 0b01010110, 0b01111000},
+    std::vector<std::uint8_t>{0b00000000,  // MSBs
+                              0b00010010, 0b00110100, 0b01010110, 0b01111000}};
+raw_and_encoded const seven{
+    std::vector<std::uint8_t>{0b00010010, 0b00110100, 0b01010110, 0b01111000,
+                              0b10011010, 0b10111100, 0b11011110},
+    std::vector<std::uint8_t>{0b00000111,  // MSBs
+                              0b00010010, 0b00110100, 0b01010110, 0b01111000,
+                              0b00011010, 0b00111100, 0b01011110}};
+raw_and_encoded const eight{
+    std::vector<std::uint8_t>{0b00010010, 0b00110100, 0b01010110, 0b01111000,
+                              0b10011010, 0b10111100, 0b11011110, 0b11110000},
+    std::vector<std::uint8_t>{
+        // block #1
+        0b00000111,  // MSBs for block #1
+        0b00010010, 0b00110100, 0b01010110, 0b01111000, 0b00011010, 0b00111100,
+        0b01011110,
+
+        // block #2
+        0b0100'0000,  // MSBs for block #2 0b0111'0000, }};
+
+}  // end anonymous namespace
+
+// NOLINTNEXTLINE
+INSTANTIATE_TEST_SUITE_P(Mcoded7, Mcoded7,
+                         testing::Values(empty, four, seven, eight));
+
+namespace {
+
+void Mcoded7RoundTrip(std::vector<std::uint8_t> const& input) {
+  auto const encoded = Mcoded7::encode(input);
+  auto const decoded = Mcoded7::decode(encoded);
+  EXPECT_THAT(decoded, testing::ContainerEq(input));
+}
+
+}  // end anonymous namespace
+
+#if defined(MCODED7_FUZZTEST) && MCODED7_FUZZTEST
+// NOLINTNEXTLINE
+FUZZ_TEST(Mcoded7, Mcoded7RoundTrip);
+#endif
+// NOLINTNEXTLINE
+TEST(Mcoded7, EmptyRoundTrip) {
+  Mcoded7RoundTrip(std::vector<std::uint8_t>{});
+}
+
+// NOLINTNEXTLINE
+TEST(Mcoded7, GoodInput) {
+  mcoded7::decoder decoder;
+  std::array<std::uint8_t, 1> output;
+  auto* out = output.data();
+  out = decoder.parse_byte(0b00000000, out);
+  EXPECT_TRUE(decoder.good());
+  out = decoder.parse_byte(0b00010010, out);
+  EXPECT_TRUE(decoder.good());
+}
+
+// NOLINTNEXTLINE
+TEST(Mcoded7, BadInput) {
+  mcoded7::decoder decoder;
+  std::array<std::uint8_t, 2> output;
+  auto* out = output.data();
+  out = decoder.parse_byte(0b00000000, out);
+  EXPECT_TRUE(decoder.good());
+  out = decoder.parse_byte(0b10010010, out);
+  EXPECT_FALSE(decoder.good())
+      << "Most significant bit was set: state should be bad";
+  out = decoder.parse_byte(0b00010010, out);
+  EXPECT_FALSE(decoder.good()) << "Expected the 'good' state to be sticky";
+}
