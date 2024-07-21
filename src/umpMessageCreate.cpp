778c5e6711ea4a00a1a69ff8a434c88745e2fcf7
Paul Bowen-Huggett <paulhuggett@mac.com>
2024-07-21 21:29:11 +0200

    Resolve implicit conversion warning.

--- src/umpMessageCreate.cpp
+++ src/umpMessageCreate.cpp
@@ -89,14 +89,10 @@ uint32_t UMPMessage::mt1SystemReset(uint8_t group) {
 
 uint32_t mt2Create(uint8_t group, uint8_t status, uint8_t channel, uint8_t val1,
                    uint8_t val2) {
-  uint32_t message;
-  message =
-      ((static_cast<std::uint8_t>(ump_message_type::m1cvm) << 4) + group + 0L)
-      << 24;
-  message += (status + channel + 0L) << 16;
-  message += (int)val1 << 8;
-  message += val2;
-  return message;
+  return (((static_cast<std::uint32_t>(ump_message_type::m1cvm) << 4) | group)
+          << 24) |
+         (static_cast<std::uint32_t>(status | channel) << 16) |
+         (static_cast<std::uint32_t>(val1) << 8) | val2;
 }
 
 uint32_t UMPMessage::mt2NoteOn(uint8_t group, uint8_t channel,
