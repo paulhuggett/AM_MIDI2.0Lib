89fa26f89e0634cc04bb6b4e2bb9958c327832f1
Paul Bowen-Huggett <paulhuggett@mac.com>
2024-07-31 20:39:25 +0200

    Remove a stray semi-colon.

--- unittests/test_umpProcessor.cpp
+++ unittests/test_umpProcessor.cpp
@@ -55,7 +55,7 @@ std::ostream& operator<<(std::ostream& os,
   case output: str = "output"; break;
   case bidirectional: str = "bidirectional"; break;
   default: str = "unknown"; break;
-  };
+  }
   return os << str;
 }
 
