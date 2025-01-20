13baa4589afc9833475c0888ed671e461df790df
Paul Bowen-Huggett <paulhuggett@mac.com>
2025-01-20 22:06:04 +0100

    Add mock expectations for the move-only test case.

--- unittests/test_lru.cpp
+++ unittests/test_lru.cpp
@@ -18,29 +18,30 @@
 
 namespace {
 
+using testing::Eq;
+using testing::InSequence;
+using testing::StrictMock;
+
 TEST(LruList, Empty) {
   midi2::lru_list<int, 4> lru;
   EXPECT_TRUE(lru.empty());
   EXPECT_EQ(lru.size(), 0);
 }
 
-class evictor_base {
+template <typename T> class evictor_base {
 public:
   virtual ~evictor_base() noexcept = default;
-  void operator()(int &v) { return this->evict(v); }
-  virtual void evict(int &v) = 0;
+  void operator()(T &v) { return this->evict(v); }
+  virtual void evict(T &v) = 0;
 };
 
-class mock_evictor : public evictor_base {
+template <typename T> class mock_evictor : public evictor_base<T> {
 public:
-  MOCK_METHOD(void, evict, (int &), (override));
+  MOCK_METHOD(void, evict, (T &), (override));
 };
-
-using testing::InSequence;
-using testing::StrictMock;
 
 TEST(LruList, AddToFull) {
-  StrictMock<mock_evictor> evictor;
+  StrictMock<mock_evictor<int>> evictor;
   midi2::lru_list<int, 4> lru;
   using int_ref = int &;
   EXPECT_THAT(static_cast<int_ref>(lru.add(1, std::ref(evictor))), 1);
@@ -61,7 +62,7 @@ TEST(LruList, AddToFull) {
 }
 
 TEST(LruList, EvictFirst) {
-  StrictMock<mock_evictor> evictor;
+  StrictMock<mock_evictor<int>> evictor;
   auto one = 1;
   EXPECT_CALL(evictor, evict(one)).Times(1);
   midi2::lru_list<int, 4> lru;
@@ -75,7 +76,7 @@ TEST(LruList, EvictFirst) {
 }
 
 TEST(LruList, TouchOneEvictTwo) {
-  StrictMock<mock_evictor> evictor;
+  StrictMock<mock_evictor<int>> evictor;
   auto two = 2;
   EXPECT_CALL(evictor, evict(two)).Times(1);
   midi2::lru_list<int, 4> lru;
@@ -90,7 +91,7 @@ TEST(LruList, TouchOneEvictTwo) {
 }
 
 TEST(LruList, Sequence) {
-  StrictMock<mock_evictor> evictor;
+  StrictMock<mock_evictor<int>> evictor;
 
   auto two = 2;
   auto three = 3;
@@ -150,17 +151,25 @@ private:
 
 TEST(LruList, MoveOnly) {
   midi2::lru_list<move_only, 2> lru;
-  auto evictor = [](move_only &) {};
+  StrictMock<mock_evictor<move_only>> evictor;
 
-  auto &node0 = lru.add(move_only{3}, evictor);
-  EXPECT_EQ(static_cast<move_only &>(node0), move_only{3});
-  auto &node1 = lru.add(move_only{5}, evictor);
-  EXPECT_EQ(static_cast<move_only &>(node1), move_only{5});
-  auto &node2 = lru.add(move_only{7}, evictor);
-  EXPECT_EQ(static_cast<move_only &>(node2), move_only{7});
-  auto &node3 = lru.add(move_only{11}, evictor);
+  move_only m3{3};
+  move_only m5{5};
+  move_only m7{7};
+  InSequence _;
+  EXPECT_CALL(evictor, evict(Eq(std::ref(m3))));
+  EXPECT_CALL(evictor, evict(Eq(std::ref(m5))));
+  EXPECT_CALL(evictor, evict(Eq(std::ref(m7))));
+
+  auto &node0 = lru.add(move_only{3}, std::ref(evictor));
+  EXPECT_EQ(static_cast<move_only &>(node0), m3);
+  auto &node1 = lru.add(move_only{5}, std::ref(evictor));
+  EXPECT_EQ(static_cast<move_only &>(node1), m5);
+  auto &node2 = lru.add(move_only{7}, std::ref(evictor));
+  EXPECT_EQ(static_cast<move_only &>(node2), m7);
+  auto &node3 = lru.add(move_only{11}, std::ref(evictor));
   EXPECT_EQ(static_cast<move_only &>(node3), move_only{11});
-  auto &node4 = lru.add(move_only{13}, evictor);
+  auto &node4 = lru.add(move_only{13}, std::ref(evictor));
   EXPECT_EQ(static_cast<move_only &>(node4), move_only{13});
 }
 
