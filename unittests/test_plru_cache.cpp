754e3e5e7111382795e7fa509702641a5535ec31
Paul Bowen-Huggett <paulhuggett@mac.com>
2025-08-29 20:25:39 +0200

    Add fuzz test

--- unittests/test_plru_cache.cpp
+++ unittests/test_plru_cache.cpp
@@ -15,6 +15,10 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
+#if defined(MIDI2_FUZZTEST) && MIDI2_FUZZTEST
+#include <fuzztest/fuzztest.h>
+#endif
+
 using midi2::plru_cache;
 
 using testing::ElementsAre;
@@ -152,17 +156,18 @@ TEST_P(PlruCacheParam, Key4x4Uint32TwoValues) {
   std::string const value = "str";
   MockFunction<std::string()> mock_function;
 
-  EXPECT_CALL(mock_function, Call()).WillOnce(Return(value)).WillOnce(Return(value)).RetiresOnSaturation();
+  EXPECT_CALL(mock_function, Call()).Times(3).WillRepeatedly(Return(value)).RetiresOnSaturation();
 
   auto const key = GetParam();
   EXPECT_EQ(cache.access(key, mock_function.AsStdFunction()), value);
+  EXPECT_EQ(cache.access(key + (1 << 2), mock_function.AsStdFunction()), value);
   EXPECT_EQ(cache.access(key + (1 << 3), mock_function.AsStdFunction()), value);
-  EXPECT_EQ(std::size(cache), 2U);
+  EXPECT_EQ(std::size(cache), 3U);
 
   // A second call with the same key doesn't create a new member.
   EXPECT_EQ(cache.access(key + (1 << 3), mock_function.AsStdFunction()), value);
   EXPECT_EQ(cache.access(key, mock_function.AsStdFunction()), value);
-  EXPECT_EQ(std::size(cache), 2U);
+  EXPECT_EQ(std::size(cache), 3U);
 }
 INSTANTIATE_TEST_SUITE_P(PlruCacheParam, PlruCacheParam, testing::Range(/*begin=*/0U, /*end=*/32U, /*step=*/4U));
 
@@ -182,38 +187,62 @@ TEST(PlruCache, Key2x8Uint16) {
   EXPECT_EQ(std::size(cache), 1U);
 }
 
+template <unsigned Sets, unsigned Ways> void NeverCrashes(std::vector<std::uint16_t> const& keys) {
+  plru_cache<std::uint16_t, std::uint16_t, Sets, Ways> cache;
+  MockFunction<std::uint16_t()> mock_function;
+  for (auto const key : keys) {
+    if (!cache.contains(key)) {
+      EXPECT_CALL(mock_function, Call()).WillOnce(Return(key)).RetiresOnSaturation();
+    }
+    EXPECT_EQ(cache.access(key, mock_function.AsStdFunction()), key);
+  }
+}
+
+#if defined(MIDI2_FUZZTEST) && MIDI2_FUZZTEST
+// NOLINTNEXTLINE
+auto NeverCrashes2x4 = NeverCrashes<2, 4>;
+FUZZ_TEST(PlruCacheFuzz, NeverCrashes2x4);
+#endif
+TEST(PlruCacheFuzz, Empty2x4) {
+  NeverCrashes<2, 4>({1, 2, 3, 4, 5, 4, 3, 2, 1});
+}
+TEST(PlruCacheFuzz, Empty2x8) {
+  NeverCrashes<2, 8>({1, 2, 3, 4, 5, 4, 3, 2, 1});
+}
+
 class counted {
 public:
-  counted() : ctr_{++count_} { actions.emplace_back(action::ctor, ctr_); }
+  enum class action { ctor, dtor };
+  using container = std::vector<std::tuple<action, unsigned>>;
+
+  explicit counted(container* const actions, unsigned ctr) : actions_{actions}, ctr_{ctr} {
+    actions->emplace_back(action::ctor, ctr_);
+  }
   counted(counted const&) = delete;
-  counted(counted&&) = delete;
+  counted(counted&&) noexcept = delete;
   ~counted() noexcept {
     try {
-      actions.emplace_back(action::dtor, ctr_);
+      actions_->emplace_back(action::dtor, ctr_);
     } catch (...) {
       // Just ignore any exceptions
     }
   }
 
   counted& operator=(counted const&) = delete;
-  counted& operator=(counted&&) = delete;
-
-  enum class action { ctor, dtor };
-  static std::vector<std::tuple<action, unsigned>> actions;
+  counted& operator=(counted&&) noexcept = delete;
 
 private:
+  container* const actions_;
   unsigned ctr_ = 0;
-  static unsigned count_;
 };
-
-unsigned counted::count_ = 0;
-std::vector<std::tuple<counted::action, unsigned>> counted::actions;
 
 TEST(PlruCache, OverFill) {
   plru_cache<unsigned, counted, 4, 2> cache;
-  using tup = decltype(counted::actions)::value_type;
 
-  auto miss = []() { return counted{}; };
+  counted::container actions;
+  using tup = decltype(actions)::value_type;
+  auto count = 0U;
+  auto miss = [&]() { return counted{&actions, ++count}; };
 
   cache.access(1, miss);
   cache.access(2, miss);
@@ -224,42 +253,42 @@ TEST(PlruCache, OverFill) {
   cache.access(7, miss);
   cache.access(8, miss);
   using enum counted::action;
-  EXPECT_THAT(counted::actions, ElementsAre(tup{ctor, 1U}, tup{ctor, 2U}, tup{ctor, 3U}, tup{ctor, 4U}, tup{ctor, 5U},
-                                            tup{ctor, 6U}, tup{ctor, 7U}, tup{ctor, 8U}));
+  EXPECT_THAT(actions, ElementsAre(tup{ctor, 1U}, tup{ctor, 2U}, tup{ctor, 3U}, tup{ctor, 4U}, tup{ctor, 5U},
+                                   tup{ctor, 6U}, tup{ctor, 7U}, tup{ctor, 8U}));
   // Accesses of items in the cache. These should now be most-recently used.
   cache.access(1, miss);
   cache.access(2, miss);
   cache.access(3, miss);
-  EXPECT_EQ(counted::actions.size(), 8U) << "Expected no new actions to have happened";
+  EXPECT_EQ(actions.size(), 8U) << "Expected no new actions to have happened";
 
   // Reset the actions.
-  counted::actions.clear();
+  actions.clear();
   // Accessing a new element will cause an eviction.
   cache.access(9, miss);
   // Check what we threw out and what was added.
-  EXPECT_THAT(counted::actions, ElementsAre(tup{dtor, 5U}, tup{ctor, 9U}));
+  EXPECT_THAT(actions, ElementsAre(tup{dtor, 5U}, tup{ctor, 9U}));
 
-  counted::actions.clear();
+  actions.clear();
   cache.access(10, miss);
-  EXPECT_THAT(counted::actions, ElementsAre(tup{dtor, 6U}, tup{ctor, 10U}));
-  counted::actions.clear();
+  EXPECT_THAT(actions, ElementsAre(tup{dtor, 6U}, tup{ctor, 10U}));
+  actions.clear();
   cache.access(11, miss);
-  EXPECT_THAT(counted::actions, ElementsAre(tup{dtor, 7U}, tup{ctor, 11U}));
-  counted::actions.clear();
+  EXPECT_THAT(actions, ElementsAre(tup{dtor, 7U}, tup{ctor, 11U}));
+  actions.clear();
   cache.access(12, miss);
-  EXPECT_THAT(counted::actions, ElementsAre(tup{dtor, 4U}, tup{ctor, 12U}));
-  counted::actions.clear();
+  EXPECT_THAT(actions, ElementsAre(tup{dtor, 4U}, tup{ctor, 12U}));
+  actions.clear();
   cache.access(13, miss);
-  EXPECT_THAT(counted::actions, ElementsAre(tup{dtor, 1U}, tup{ctor, 13U}));
-  counted::actions.clear();
+  EXPECT_THAT(actions, ElementsAre(tup{dtor, 1U}, tup{ctor, 13U}));
+  actions.clear();
   cache.access(14, miss);
-  EXPECT_THAT(counted::actions, ElementsAre(tup{dtor, 2U}, tup{ctor, 14U}));
-  counted::actions.clear();
+  EXPECT_THAT(actions, ElementsAre(tup{dtor, 2U}, tup{ctor, 14U}));
+  actions.clear();
   cache.access(15, miss);
-  EXPECT_THAT(counted::actions, ElementsAre(tup{dtor, 3U}, tup{ctor, 15U}));
-  counted::actions.clear();
+  EXPECT_THAT(actions, ElementsAre(tup{dtor, 3U}, tup{ctor, 15U}));
+  actions.clear();
   cache.access(16, miss);
-  EXPECT_THAT(counted::actions, ElementsAre(tup{dtor, 8U}, tup{ctor, 16U}));
+  EXPECT_THAT(actions, ElementsAre(tup{dtor, 8U}, tup{ctor, 16U}));
 }
 
 }  // end anonymous namespace
