e485c84c99757239057bd2280b3c7c6e00f59406
Paul Bowen-Huggett <paulhuggett@mac.com>
2026-03-09 19:58:54 +0100

    Prototype readthedocs integration

---
+++ docs/conf.py
@@ -0,0 +1,42 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+import subprocess
+
+project = 'midi2y'
+copyright = '2025, Paul Bowen-Huggett'
+author = 'Paul Bowen-Huggett'
+version = '0.1'
+release = version
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+     'breathe',
+     'sphinx.ext.autosectionlabel',
+     'sphinx.ext.graphviz',
+     'sphinx_design'
+]
+
+# Breathe configuration
+breathe_projects = { project: './xml/' }
+breathe_default_project = project
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo'
+html_static_path = ['_static']
+
+if os.environ.get('READTHEDOCS', None) == 'True':
+     subprocess.call('cd ..; doxygen', shell=True)
---
+++ docs/index.rst
@@ -0,0 +1,10 @@
+Welcome to midi2!
+===================
+
+The AM MIDI 2.0 Library is a general-purpose C++ library that provides the building blocks, processing, and translations needed for MIDI 2.0 devices and applications. It enables bidirectional conversion between MIDI 1.0 bytestreams and Universal MIDI Packet (UMP) format, supports processing and constructing UMP streams, and handles MIDI Capability Inquiry (MIDI-CI) messages.
+
+Installation
+------------
+
+Usage
+-----
